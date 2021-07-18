40a7a42252d372ad4d868afb34619c7580c507a5
Raz <raz_st@hotmail.com>
2021-07-18 14:52:21 +0300

    read states, focus values as list

--- scp_main.py
+++ scp_main.py
@@ -7,7 +7,6 @@ from pydrive.drive import GoogleDrive
 import yaml
 from system_state import SystemState 
 from system_state import CameraConfiguration
-from system_state import FocusPosition
 import logging
 from camera_handler import CameraHandler
 from gdrive_handler import GDriveHandler
@@ -19,14 +18,20 @@ states_over_time=[]
 # get the system's current state based on current time
 def get_current_state():
     current_time = time.time()
-    for i in range(len(states_over_time)):
-        d = time.mktime(datetime.strptime(states_over_time[i][0], "%Y/%m/%d %H:%M:%S").timetuple())
-        if d>current_time:
-            if i==0:
-                logging.error("states_over_time starts after current time, taking first value")
-                return states_over_time[i-1][1]
-            else:
-                return states_over_time[i-1][1]
+    current_state="NA"
+    # go over all states and find the one we sould use now
+    for state in reversed(states_over_time):
+        the_date = time.mktime(datetime.strptime(state[0], "%Y/%m/%d %H:%M:%S").timetuple())
+        logging.debug("current time:%d the_date:%d",current_time , the_date)
+        if current_time > the_date:
+            current_state = state[1]
+            break
+    if current_state=="NA":
+        logging.info("cant find states over time, taking first state")
+        current_state = state[1]
+            
+    logging.info("current state found:%s",current_state)
+    return system_states[current_state]
 
 def setup_logging():
     logging.basicConfig(
@@ -49,15 +54,13 @@ def get_system_states():
         name = raw_system_states[i]["name"]
         cam_conf=raw_system_states[i]["camera_configuration"]
         if cam_conf==None:
-            state = SystemState(CameraConfiguration(-1,-1,-1,FocusPosition(-1,-1,-1)),name)  
+            state = SystemState(None,name)  
         else:
             image_frequency_min = cam_conf["image_frequency_min"]
-            min_f = cam_conf["focus_position"]["min"]
-            max_f = cam_conf["focus_position"]["max"]
-            step_f = cam_conf["focus_position"]["step"]
+            focus_position = cam_conf["focus_position"]
             exposure = cam_conf["exposure"]
             iso =cam_conf["ISO"]
-            state = SystemState(CameraConfiguration(image_frequency_min,exposure,iso,FocusPosition(min_f,max_f,step_f)),name)  
+            state = SystemState(CameraConfiguration(image_frequency_min,exposure,iso,focus_position),name)  
         state.print_values()
         system_states[name]=state
          
@@ -80,7 +83,7 @@ def getGDrive_folder_id():
     return folder_id
 
 # get configuration data from yaml file
-def get_settings():
+def get_states_settings():
     get_system_states()
     get_state_over_time()
     
@@ -92,43 +95,37 @@ def get_file_name ():
 def take_pic_and_upload(camera,g_drive,focus):
     camera.change_focus(focus)
     full_path_file_name,title_name = camera.take_pic(get_file_name())
-    g_drive.upload_file(full_path_file_name,title_name)
-
+    
+    try:
+        g_drive.upload_file(full_path_file_name,title_name)
+    except:
+        logging.error("timeout while uploading file to G Drive")
 
 def main():
     setup_logging()
     logging.info('*** Start ***')
-    # get_settings()
-
+    # get handler to G-Drive
     g_drive_handler = GDriveHandler(getGDrive_folder_id())
+    get_states_settings()
+    # get handler for the cameras
     camera = CameraHandler('A',focus=512)
 
     while(True):
+        state = get_current_state()
+        state.print_values()
+
         # take take pictures from camera A
         camera.change_active_camera("A")
-        take_pic_and_upload(camera,g_drive_handler,512)
-        take_pic_and_upload(camera,g_drive_handler,260)
-        
+        take_pic_and_upload(camera,g_drive_handler,250)
+        take_pic_and_upload(camera,g_drive_handler,150)        
 
         # take take pictures from camera C
         camera.change_active_camera("C")
-        take_pic_and_upload(camera,g_drive_handler,512)
-        take_pic_and_upload(camera,g_drive_handler,260)
+        take_pic_and_upload(camera,g_drive_handler,250)
+        take_pic_and_upload(camera,g_drive_handler,150)
     
         logging.info('going to sleep...')
         time.sleep(60*10)
-
-    # while (True):
-    #     get_current_state()
-    #     time.sleep(1)
-
-    # start_LED ()
-    # for f in range(100,101,1):
-    #     take_pic(0,False,f,5,5)
-    #     take_pic (1,True,f,10,10)
-    # stop_LED()
-    logging.info('*** End ***')
-
 
 if __name__ == "__main__":
     main()
--- system_state.py
+++ system_state.py
@@ -11,38 +11,31 @@ class SystemState:
         self.name = name
     def print_values(self):
         logging.info("System States values:")
-        logging.info("*** name:%s ***", self.name)
-        logging.info("image_frequency_min:%d",self.camera_configuration.image_frequency_min)
-        logging.info("focus_position:")
-        logging.info("\tmin:%d",self.camera_configuration.focus_position.min)
-        logging.info("\tmax:%d",self.camera_configuration.focus_position.max)
-        logging.info("\tstep:%d",self.camera_configuration.focus_position.step)
-        logging.info("exposure:%d",self.camera_configuration.exposure)
-        logging.info("ISO:%d",self.camera_configuration.iso)
+        logging.info("name:%s", self.name)
+        if self.camera_configuration != None:
+            logging.info("image_frequency_min:%d",self.camera_configuration.image_frequency_min)
+            logging.info("focus_position:")
+            for fp in self.camera_configuration.focus_position:
+                logging.info("* %d",fp)
+            logging.info("exposure:%d",self.camera_configuration.exposure)
+            logging.info("ISO:%d",self.camera_configuration.iso)
+        else:
+            logging.info("camera_configuration:None")
 
 class CameraConfiguration:
     image_frequency_min = 5 # How often should we take images (minutes)
     exposure = 10
     iso = 10
-    focus_position = None
+    focus_position = []
     def __init__(self,image_frequency_min,exposure,iso,focus_position):
         self.image_frequency_min = image_frequency_min
         self.exposure = exposure
         self.iso = iso
         self.focus_position = focus_position
 
-class FocusPosition:
-    min=0
-    max=1023
-    step=100
-    def __init__(self,min,max,step):
-        self.min = min
-        self.max = max
-        self.step = step
-
 if __name__ == "__main__":
-    s = SystemState(CameraConfiguration(6,20,30,FocusPosition(10,100,5)))    
+    s = SystemState(CameraConfiguration(6,20,30,[10,100,5]),"day_sun")    
     print(s.camera_configuration.iso)
-    print(s.camera_configuration.focus_position.step)
+    print(s.camera_configuration.focus_position)
 
     
