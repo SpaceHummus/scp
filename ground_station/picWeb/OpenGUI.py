8e424b61789dc56670668a00ce7764bae7940c84
AvivLabsGit <92829165+AvivLabsGit@users.noreply.github.com>
2021-12-19 15:26:15 +0200

    Update OpenGUI.py

--- ground_station/picWeb/OpenGUI.py
+++ ground_station/picWeb/OpenGUI.py
@@ -1,3 +1,3 @@
 from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
-app.run(host='http://mc-avivlabs.duckdns.org', port=8090)
+app.run(host='mc-avivlabs.duckdns.org', port=8090)
