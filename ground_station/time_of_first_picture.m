89b8f01d53648473cfd73be7c4f48bc03dc67739
MyYo <MyYo@users.noreply.github.com>
2022-01-15 16:42:53 -0800

    [Non Flight Software] Typo

--- ground_station/time_of_first_picture.m
+++ ground_station/time_of_first_picture.m
@@ -2,4 +2,4 @@ function t0 = time_of_first_picture(image_directory)
 % Figure out what time was the first picture taken (start of experiment)
 
 ds =  fileDatastore([image_directory '\*.jpg'],'ReadFcn',@imread);
-t0 = time_picture_was_taken(ds1.Files{1});
+t0 = time_picture_was_taken(ds.Files{1});
