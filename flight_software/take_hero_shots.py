3f6913033f2ab1689858f6e2dd0609e3caef1eb3
MyYo <MyYo@users.noreply.github.com>
2022-02-12 19:40:36 -0800

    Hero shots formatting change

--- flight_software/take_hero_shots.py
+++ flight_software/take_hero_shots.py
@@ -25,22 +25,24 @@ class HeroShots:
         self.cam = camera_handler_high_level.CameraHandlerHighLevel()
         self.cam.init_camera_handler()
 
-    def hero_shot_cameras_abcd(self, hero_name, date_name, r_g_b_fr_mw):
+    def hero_shot_cameras_Zabcd(self, hero_name, date_name, r_g_b_fr_mw):
         logging.info("Take {0}".format(hero_name))
+        file_name_prefix="{1}_HeroShot_{0}".format(hero_name,date_name)
+        
         self.cam.take_pic_all_focus('A',self.camera_A_focus_setting,
-            file_name_prefix="HeroShot_{0}_{1}".format(hero_name,date_name),
+            file_name_prefix=file_name_prefix,
             r_g_b_fr_mw = r_g_b_fr_mw)
         time.sleep(1)
         self.cam.take_pic_all_focus('B',self.camera_B_focus_setting,
-            file_name_prefix="HeroShot_{0}_{1}".format(hero_name,date_name),
+            file_name_prefix=file_name_prefix,
             r_g_b_fr_mw = r_g_b_fr_mw)
         time.sleep(1)
         self.cam.take_pic_all_focus('C',self.camera_C_focus_setting,
-            file_name_prefix="HeroShot_{0}_{1}".format(hero_name,date_name),
+            file_name_prefix=file_name_prefix,
             r_g_b_fr_mw = r_g_b_fr_mw)
         time.sleep(1)
         self.cam.take_pic_all_focus('D',self.camera_D_focus_setting,
-            file_name_prefix="HeroShot_{0}_{1}".format(hero_name,date_name),
+            file_name_prefix=file_name_prefix,
             r_g_b_fr_mw = r_g_b_fr_mw)
         time.sleep(1)
 
@@ -69,7 +71,8 @@ if __name__ == "__main__":
     os.system('./stop_scp_main.sh')
     
     hs.hero_shot_cameras_abcd('WhiteLight',date_name,[100, 100, 100, 0, 0])
-    hs.hero_shot_cameras_abcd('HotRed',date_name,[0, 0, 0, 10, 0])
+    hs.hero_shot_cameras_abcd('HotRed1',date_name,[0, 0, 0, 10, 0])
+    hs.hero_shot_cameras_abcd('HotRed2',date_name,[0, 0, 0, 10, 1])
     hs.hero_shot_cameras_abcd('Backlight1',date_name,[0, 0, 0, 0, 1])
     hs.hero_shot_cameras_abcd('Backlight2',date_name,[100, 100, 100, 0, 1])
  
